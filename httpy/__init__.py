9fc7f32ba79e2f68a4f023203e6266736754db8c
Adam Jenča <jenca.adam@gmail.com>
2022-04-04 20:53:28 +0200

    Moved VERSION to 1.0.0

--- httpy/__init__.py
+++ httpy/__init__.py
@@ -22,7 +22,7 @@ import ctypes
 
 HTTPY_DIR=pathlib.Path.home()/'.cache/httpy'
 os.makedirs(HTTPY_DIR/'sites',exist_ok=True)
-version='0.0.dev0'
+version='1.0.0'
 urlpattern=re.compile('^(?P<scheme>[a-z]+)://(?P<host>[^/:]*)(:(?P<port>(\d+)?))?/?(?P<path>.*)$')
 statuspattern=re.compile(br'(?P<version>.*)\s*(?P<status>\d{3})\s*(?P<reason>[^\r\n]*)')
 context=ssl.create_default_context()
