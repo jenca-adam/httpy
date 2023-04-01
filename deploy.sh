4c9e9333758d7532edd2eedcc187fb750e7d8b58
Adam Jenča <jenca.adam@gmail.com>
2023-04-01 15:38:13 +0200

    1.8.1

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
+echo "deploying"
+for i in $(dir dist);do echo "moving $i";mv dist/$i official_dist/$i;done
+echo "building dist"
 python setup.py sdist bdist bdist_wheel
+echo "git stuff"
+git add --all -v
+git commit -m "$(python3 -c 'print(__import__("httpy").__version__)')"
+git push
