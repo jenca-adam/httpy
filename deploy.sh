3b0b38a8b9d45a7e2e6b6acf1eec76e424ed6f88
Adam Jenča <jenca.adam@gmail.com>
2023-04-01 15:46:03 +0200

    1.8.1 bump (automated commit)

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
+VER=$(python3 -c 'print(__import__("httpy").__version__)')
 echo "deploying"
 for i in $(dir dist);do echo "moving $i";mv dist/$i official_dist/$i;done
 echo "building dist"
 python setup.py sdist bdist bdist_wheel
 echo "git stuff"
 git add --all -v
-git commit -m "$(python3 -c 'print(__import__("httpy").__version__)')"
+git commit -m "$VER bump (automated commit)"
 git push
+clear
+echo "========="
+echo "IMPORTANT"
+echo "========="
+echo "Use twine to upload [y*]?" -n
+read book
+if [ $book == "y" ];then clear;twine dist/*.whl "dist/httpy-$VER.tar.gz";fi
+clear
+echo "Goodbye!"
+
