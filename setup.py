5e505da126f7aa53fff245ccdd9370422bded60b
Adam Jenča <jenca.adam@gmail.com>
2023-10-01 10:23:41 +0200

    1.8.2 bump (automated commit)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     author="Adam Jenca",
     description="Lightweight socket-based HTTP(s) and WebSocket client.",
     long_description=ld,
-    version="1.8.1",
+    version="1.8.2",
     long_description_content_type="text/markdown",
     packages=["httpy"],
     author_email="jenca.a@gjh.sk",
