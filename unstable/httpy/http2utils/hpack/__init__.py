23ff65466c75523f9103a1954565b593fdab27a9
Adam Jenča <jenca.adam@gmail.com>
2023-10-11 21:12:52 +0200

    move all unstable code from main branch

--- unstable/httpy/http2utils/hpack/__init__.py
+++
@@ -1,1 +0,0 @@
-from .hpack import *
--- unstable/httpy/http2utils/hpack/errors.py
+++
@@ -1,7 +0,0 @@
-from httpy import HTTPyError
-class HPackError(HTTPyError):pass
-class EncodingError(HPackError):pass
-class DecodingError(HPackError):pass
-class HuffmanError(HPackError):pass
-class HuffmanDecodingError(HuffmanError,DecodingError):pass
-class HuffmanEncodingError(HuffmanError,EncodingError):pass
